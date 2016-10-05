63c3ae98989e0254f04453b460afb3eab3f173b9
Ted Shaffer <ted@brightsign.biz>
2016-10-05 06:55:03 -0700

    skeleton files to get started.

---
+++ src/bsDmClasses.js
@@ -0,0 +1,3 @@
+/**
+ * Created by tedshaffer on 10/5/16.
+ */
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tedshaffer on 10/5/16.
+ */
+import {combineReducers} from 'redux';
+
+const bsdmReducer = combineReducers({
+});
+
+export default bsdmReducer;
