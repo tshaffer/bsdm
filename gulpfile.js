402d3b8ecb2807f5ace3a350c4bdfbab7840e4ad
Ted Shaffer <ted@brightsign.biz>
2016-10-06 08:16:25 -0700

    compile all files to dist, next step...

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,19 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () => {
-    return gulp.src('src/bsDmActions.js')
+    gulp.src('src/*.js')
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(gulp.dest('dist'));
+    gulp.src('src/utilities/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/utilities'));
+    gulp.src('src/reducers/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/reducers'));
 });
