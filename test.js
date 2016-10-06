37d6030dcc21353c00f2f6b2fb6a2b79c0b4268d
Ted Shaffer <ted@brightsign.biz>
2016-10-05 17:54:43 -0700

    add 2nd media state

--- test.js
+++ test.js
@@ -20,6 +20,9 @@ const contentItem1 = { id: "", name: "ItemName1", type: "media", media: mediaObj
 console.log("contentItem1");
 console.log(contentItem1);
 
+const mediaObject2 = {path: '/testFiles/image2.jpg', mediaType: "image"};
+const contentItem2 = { id: "", name: "ItemName2", type: "media", media: mediaObject2 };
+
 store.dispatch(baNewSign('TestSign', "v1920x1080x60p"));
 
 let state = store.getState();
@@ -41,7 +44,7 @@ let zone1Container = zone1.containerObject;
 let msAction = store.dispatch(baAddMediaState('State1', zone1Container, contentItem1));
 
 state = store.getState();
-console.log("state after adding media state:");
+console.log("state after adding media state1:");
 console.log(state);
 
 let mediaState1Id = msAction.id;
@@ -62,7 +65,13 @@ state = store.getState();
 console.log("state after adding updating zone:");
 console.log(state);
 
-
 zone1 = getZoneById(store.getState(), {id: zone1Id});
 console.log("zone1:");
 console.log(zone1);
+
+msAction = store.dispatch(baAddMediaState('State2', zone1Container, contentItem2));
+
+state = store.getState();
+console.log("state after adding media state2:");
+console.log(state);
+
